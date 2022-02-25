500afed3c6d78ba2e3a2e3abd6a56b21cded1fe6
Antoha-x <272acy@gmail.com>
2022-02-26 02:28:20 +0500

    Test-3 complete

--- MainClass.py
+++ MainClass.py
@@ -1,8 +1,12 @@
 class MainClass:
     __class_number = 20
+    __class_string = "hello, world"
 
     def get_local_number(self):
         return 14
 
     def get_class_number(self):
         return self.__class_number
+
+    def get_class_string(self):
+        return self.__class_string
--- MainClassTest.py
+++ MainClassTest.py
@@ -4,6 +4,7 @@ from MainClass import MainClass
 class TestMainClass:
     expected_value_test_1 = 14
     expected_value_test_2 = 45
+    expected_value_test_3 = ["Hello", "hello"]
     mc = MainClass()
 
 
@@ -12,3 +13,7 @@ class TestMainClass:
 
     def test_get_class_number(self):
         assert self.mc.get_class_number() > self.expected_value_test_2, "getClassNumber return less than 45"
+
+    def test_get_class_string(self):
+        assert any(list(map(lambda x: x in self.mc.get_class_string(), self.expected_value_test_3))), \
+            f"getClassString does not contain the substring {self.expected_value_test_3}"
