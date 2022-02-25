858d5cec681d87503442d4630f861489c3bdfc95
Antoha-x <272acy@gmail.com>
2022-02-26 00:04:19 +0500

    Test-2 complete

--- MainClassTest.py
+++ MainClassTest.py
@@ -1,8 +1,14 @@
 import pytest
-from MainClass import get_local_number
+from MainClass import MainClass
 
 class TestMainClass:
-    expected_value = 14
+    expected_value_test_1 = 14
+    expected_value_test_2 = 45
+    mc = MainClass()
 
-    def test_getLocal_number(self):
-        assert get_local_number() == self.expected_value, "getLocalNumber return not 14"
+
+    def test_get_local_number(self):
+        assert self.mc.get_local_number() == self.expected_value_test_1, "getLocalNumber return not 14"
+
+    def test_get_class_number(self):
+        assert self.mc.get_class_number() > self.expected_value_test_2, "getClassNumber return less than 45"
