712f542aa918fd6fa97eb5997fd1ed19bfe75457
Antoha-x <272acy@gmail.com>
2022-03-06 02:23:29 +0500

    Exercise 1 complete

---
+++ src/tests/first/FirstTest.py
@@ -0,0 +1,4 @@
+class TestFirst:
+
+    def test_first(self, set_up_and_tear_down):
+        print("First test run")
---
+++ src/tests/first/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from appium import webdriver
+
+
+@pytest.fixture()
+def set_up_and_tear_down():
+    desired_capabilities = {
+        "platformName": "Android",
+        "deviceName": "emulator-5554",
+        "platformVersion": "8.0",
+        "automationName": "Appium",
+        "appPackage": "org.wikipedia",
+        "appActivity": ".main.MainActivity",
+        "app": "C:\\LearnQA\\avt-mob-50-python\\apks\\\org.wikipedia.apk"
+    }
+
+    driver = webdriver.Remote(command_executor= 'http://127.0.0.1:4723/wd/hub',
+                              desired_capabilities=desired_capabilities)
+    yield driver
+
+    driver.quit()
